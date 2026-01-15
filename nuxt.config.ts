d2aa301a341dfedeea2a7cd40889e00339dae617
Konstantin Kostadinov <kkostadinov@weband.bg>
2026-01-15 17:52:33 +0200

    Nuxt setup with postgre

---
+++ nuxt.config.ts
@@ -0,0 +1,18 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules: [
+    '@nuxt/ui'
+  ],
+
+  devtools: {
+    enabled: true
+  },
+
+  css: ['~/assets/css/main.css'],
+
+  compatibilityDate: '2025-01-15',
+
+  runtimeConfig: {
+    databaseUrl: process.env.DATABASE_URL
+  },
+})
